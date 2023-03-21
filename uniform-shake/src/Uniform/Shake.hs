071dda2d894e9f8c2a59edcd0fcd32151292d9e1
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2023-03-21 16:28:13 +0100

    removed DNB as a rule to exclude files

--- uniform-shake/src/Uniform/Shake.hs
+++ uniform-shake/src/Uniform/Shake.hs
@@ -103,15 +103,15 @@ runErr2action op = liftIO $ do
 -- throwAction :: Text -> Action () 
 -- throwAction msg = liftIO . throwIO $ msg
 
-getFilesToBake :: Text -> Path Abs Dir -> [FilePattern] 
-        -> Action [Path Rel File]
--- | get all files according to the FilePattern (see Shake docs)
--- in the given directory
--- but excludes all filepath which contain one of the strings in 
--- the first argument to allow directories which are not baked
+-- getFilesToBake :: Text -> Path Abs Dir -> [FilePattern] 
+--         -> Action [Path Rel File]
+-- -- | get all files according to the FilePattern (see Shake docs)
+-- -- in the given directory
+-- -- but excludes all filepath which contain one of the strings in 
+-- -- the first argument to allow directories which are not baked
 
-getFilesToBake exclude d p = do
-    res :: [Path Rel File] <- getDirectoryFilesP d p
-    let filtered = filter (not . (isInfixOf' exclude) . s2t .toFilePath ) res
-    -- putIOwords [unlines' $ map (s2t . toFilePath) filtered]
-    return   filtered
+-- getFilesToBake exclude d p = do
+--     res :: [Path Rel File] <- getDirectoryFilesP d p
+--     let filtered = filter (not . (isInfixOf' exclude) . s2t .toFilePath ) res
+--     -- putIOwords [unlines' $ map (s2t . toFilePath) filtered]
+--     return   filtered
